4cef90454a907375848a24ef6a84976b47d27b3c
Bloody-Mary <37978402+Bloody-Mary@users.noreply.github.com>
2023-06-13 17:53:05 +0300

    Update _Main.java

--- itgirlschool/src/main/java/ru/babushkina/week3/adtask/_main/_Main.java
+++ itgirlschool/src/main/java/ru/babushkina/week3/adtask/_main/_Main.java
@@ -1,7 +1,12 @@
 package ru.babushkina.week3.adtask._main;
 
+import ru.babushkina.week3.adtask.model.Bear;
+import ru.babushkina.week3.adtask.util.EventSimulator;
+
 public class _Main {
     public static void main(String[] args) {
-
+        Bear bear = new Bear();
+        EventSimulator eventSimulator = new EventSimulator();
+        eventSimulator.launchSimulator(bear);
     }
 }
